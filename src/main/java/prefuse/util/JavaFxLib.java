905cb490f0c91ab5b085b028dac5b19f7a72e587
gedeffe <barbier.gabriel@gmail.com>
2015-08-23 14:21:07 +0200

    #1 Start implementation of JavaFxLib.
    A library to draw a Prefuse visual item with a GraphicsContext. It is
    only a first step during migration. We would like to reach a point where
    GraphicsContext becomes useless and we would be able to use javaFX
    shapes directly.

--- src/main/java/prefuse/util/JavaFxLib.java
+++ src/main/java/prefuse/util/JavaFxLib.java
@@ -1,7 +1,17 @@
 package prefuse.util;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.shape.ArcTo;
+import javafx.scene.shape.ClosePath;
+import javafx.scene.shape.CubicCurveTo;
+import javafx.scene.shape.HLineTo;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.PathElement;
+import javafx.scene.shape.QuadCurveTo;
 import javafx.scene.shape.Shape;
+import javafx.scene.shape.VLineTo;
 
 /**
  * Library to render a Shape used to store and compute information with a
@@ -32,6 +42,52 @@ public class JavaFxLib {
 	 *            instance associated to main canvas.
 	 */
 	public static void drawShape(final Shape shape, final GraphicsContext graphicsContext) {
-
+		if (shape instanceof Path) {
+			drawPath((Path) shape, graphicsContext);
+		}
 	}
+
+	/**
+	 * Derived method from https://github.com/Xenoage/Zong/blob/
+	 * 0f0d6a721fb027d1d70dc049d3fe0c2350be4596/renderer-javafx/src/com/xenoage/
+	 * zong/renderer/javafx/symbols/JfxPath.java
+	 *
+	 * @param path
+	 * @param context
+	 */
+	public static void drawPath(final Path path, final GraphicsContext context) {
+		context.beginPath();
+		for (final PathElement e : path.getElements()) {
+			if (e instanceof ClosePath) {
+				context.closePath();
+			} else if (e instanceof CubicCurveTo) {
+				final CubicCurveTo c = (CubicCurveTo) e;
+				context.bezierCurveTo(c.getControlX1(), c.getControlY1(), c.getControlX2(), c.getControlY2(), c.getX(),
+						c.getY());
+			} else if (e instanceof LineTo) {
+				final LineTo l = (LineTo) e;
+				context.lineTo(l.getX(), l.getY());
+			} else if (e instanceof MoveTo) {
+				final MoveTo m = (MoveTo) e;
+				context.moveTo(m.getX(), m.getY());
+			} else if (e instanceof QuadCurveTo) {
+				final QuadCurveTo q = (QuadCurveTo) e;
+				context.quadraticCurveTo(q.getControlX(), q.getControlY(), q.getX(), q.getY());
+			} else if (e instanceof ArcTo) {
+				final ArcTo q = (ArcTo) e;
+				// well, I am not sure about this backport ...
+				context.arcTo(q.getX(), q.getY(), q.getRadiusX(), q.getRadiusY(), q.getXAxisRotation());
+			} else if (e instanceof HLineTo) {
+				final HLineTo q = (HLineTo) e;
+				// how to draw an horizontal line if we don't know where we are
+				// ?
+				context.lineTo(q.getX(), 0);
+			} else if (e instanceof VLineTo) {
+				final VLineTo q = (VLineTo) e;
+				// how to draw a vertical line if we don't know where we are ?
+				context.lineTo(0, q.getY());
+			}
+		}
+	}
+
 }
